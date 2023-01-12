cff573bde62e36ae343334e1008b7aba95f5c16f
enovak89 <enovak89@mail.ru>
2023-01-12 10:53:25 +0300

    Task4-5

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
+        task5();
+        task6();
     }
+
         public static void task1 () {
         var dog = 8.0;
         var cat = 3.6;
@@ -11,6 +15,7 @@ public class Main {
         System.out.println("Задача 1");
         System.out.println(dog + " " + cat + " " + paper);
         }
+
         public static void task2 () {
         var dog = 8.0;
         var cat = 3.6;
@@ -21,6 +26,7 @@ public class Main {
             paper = paper + 4;
             System.out.println(dog + " " + cat + " " + paper);
         }
+
         public static void task3 () {
         var dog = 12.0;
         var cat = 7.6;
@@ -31,4 +37,26 @@ public class Main {
         paper = paper - 7639;
         System.out.println(dog + " " + cat + " " + paper);
         }
+
+        public static void task4 () {
+        var friend = 19;
+        System.out.println("Задача 4" + "\n" + friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 3;
+        System.out.println(friend);
+        }
+
+        public static void task5 () {
+        var frog = 3.5;
+        System.out.println("Задача 5" + "\n" + frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        }
+
+        public static void task6 () {}
 }
