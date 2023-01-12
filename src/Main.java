da1ab3026242ac636244d91142d0a6d3ba39db35
enovak89 <enovak89@mail.ru>
2023-01-12 10:29:22 +0300

    Task1-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
     }
+        public static void task1 () {
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("Задача 1");
+        System.out.println(dog + " " + cat + " " + paper);
+        }
+        public static void task2 () {
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("Задача 2");
+            cat = cat + 4;
+            dog = dog + 4;
+            paper = paper + 4;
+            System.out.println(dog + " " + cat + " " + paper);
+        }
+        public static void task3 () {
+        var dog = 12.0;
+        var cat = 7.6;
+        var paper = 763793;
+        System.out.println("Задача 3");
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog + " " + cat + " " + paper);
+        }
 }
