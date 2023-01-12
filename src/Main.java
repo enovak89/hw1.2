6564203f307a4a0d88f5349e22164b4889e444d3
enovak89 <enovak89@mail.ru>
2023-01-12 20:04:11 +0300

    Task9-14

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         task6();
         task7();
         task8();
+        task9();
     }
 
         public static void task1 () {
@@ -82,5 +83,39 @@ public class Main {
         var hoursNum = emplNum2 * 8;
         System.out.println("Задача 8" + "\n" + "Всего работников в компании - " + emplNum + " человек");
         System.out.println("Если в компании работает " + emplNum2 + " человек, то всего " + hoursNum + " часов работы может быть поделено между сотрудниками");
+        }
+
+        public static void task9 () {
+        System.out.println("\n" + "Задачи повышенной сложности" + "\n" + "Выходит " + 47 / 13 + " и " + 47 % 13 + " в остатке");
+        var a = 44;
+        var b = a / 10 + a % 10;
+        System.out.println(b);
+        var y = 345;
+        var x = y - (y / 100) * 100;
+        var z = y / 100 + x / 10 + y % 10;
+        System.out.println(z);
+        var c = 4899 + 157 - (4899 + 157) * 10 / 100;
+        System.out.println(c + " " + (4899 + 157) / 10d);
+
+        int orangePrice = 73;
+        int orangeDiscount = 15;
+        int bananaPrice = 95;
+        var sum = bananaPrice + orangePrice - ((orangePrice * orangeDiscount) / 100f);
+        System.out.println(sum);
+
+        var dDiscount = 2870 - 2870 * 50 / 100d;
+        int bread = 27;
+        var summary = dDiscount * 7 + bread;
+        System.out.println(summary + " " + dDiscount);
+
+        double shamp = 436 * 2.5;
+        double mush = 578 * 0.95 * 3.75;
+        System.out.println(shamp + mush);
+
+        double h = 0.4;
+        double g = 0.87;
+        double num1 = 11 * h + g;
+        double num2 = (h + 10 * g) - 15 * (h / g);
+        System.out.println(num1 + " " + num2);
         }
 }
